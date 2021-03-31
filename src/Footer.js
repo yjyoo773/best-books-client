e86c3e742626f00fde4274d2d1d594eccca56f37
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-31 11:06:11 -0700

    set footer to bottom

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 
 class Footer extends React.Component {
   render() {
-    return(
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand>&copy; Best Books</Navbar.Brand>
-    </Navbar>
-    )
+    return (
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        style={{ position: "absolute", left: "0", bottom: "0", right: "0" }}
+      >
+        <Navbar.Brand>&copy; Best Books</Navbar.Brand>
+      </Navbar>
+    );
   }
 }
 
